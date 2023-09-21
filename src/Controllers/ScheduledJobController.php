67498d8eaa5a5514e09680dc7637535164beaa8f
Richan Fongdasen <richan-fongdasen@users.noreply.github.com>
2023-09-21 12:42:34 +0000

    Increase the max execution time and memory limit

--- src/Controllers/ScheduledJobController.php
+++ src/Controllers/ScheduledJobController.php
@@ -16,14 +16,38 @@ class ScheduledJobController extends BaseController
     use ValidatesRequests;
 
     /**
+     * Increase the max execution time when handling a queued job.
+     */
+    protected function increaseExecutionTime(): void
+    {
+        ini_set('max_execution_time', 0);
+        set_time_limit(0);
+    }
+
+    /**
+     * Increase the memory limit when handling a queued job.
+     */
+    protected function increaseMemoryLimit(): void
+    {
+        ini_set('memory_limit', '-1');
+    }
+
+    /**
      * Handle scheduled job event invocation.
      *
      * @return \Illuminate\Http\JsonResponse
      */
     public function index(): JsonResponse
     {
+        $this->increaseExecutionTime();
+        $this->increaseMemoryLimit();
+
         Artisan::call('schedule:run');
 
-        return response()->json(['info' => 'The scheduled job has completed.']);
+        $response = [
+            'info'   => 'The scheduled job has completed.',
+        ];
+
+        return response()->json($reponse);
     }
 }
