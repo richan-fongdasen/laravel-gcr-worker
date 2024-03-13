54a00fc270f972757a6481f0dbeca929fc757c7a
Richan Fongdasen <richan.fongdasen@gmail.com>
2024-03-13 17:52:38 +0700

    Add support for Laravel 11

--- tests/Feature/PubSubEventHandlingTest.php
+++ tests/Feature/PubSubEventHandlingTest.php
@@ -3,28 +3,29 @@
 namespace RichanFongdasen\GCRWorker\Tests\Feature;
 
 use Illuminate\Support\Facades\Cache;
+use PHPUnit\Framework\Attributes\Test;
 use RichanFongdasen\GCRWorker\Facade\GcrQueue;
 use RichanFongdasen\GCRWorker\Tests\TestCase;
 
 class PubSubEventHandlingTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function it_will_return_403_on_unauthorized_pubsub_request()
     {
-        $data = json_decode(file_get_contents(dirname(__DIR__, 2).'/dummies/message.json'), true);
+        $data = json_decode(file_get_contents(dirname(__DIR__, 2) . '/dummies/message.json'), true);
 
         $this->postJson('/gcr-worker/pub-sub/event-handler', $data)
             ->assertStatus(403);
     }
 
-    /** @test */
+    #[Test]
     public function it_can_handle_pubsub_invocation_as_expected()
     {
         GcrQueue::fake();
 
         config(['gcr-worker.allow_event_invocation' => true]);
 
-        $data = json_decode(file_get_contents(dirname(__DIR__, 2).'/dummies/message.json'), true);
+        $data = json_decode(file_get_contents(dirname(__DIR__, 2) . '/dummies/message.json'), true);
 
         self::assertNull(Cache::get('dummy-job-status'));
 
@@ -32,8 +33,8 @@ class PubSubEventHandlingTest extends TestCase
             ->assertStatus(200)
             ->assertJsonFragment(['info' => 'The Pub/Sub queued job has completed.']);
 
-//        GcrQueue::assertAcknowledgedMessagesCount(1);
-//        GcrQueue::assertMessageHasAcknowledged('1777817206939726');
+        //        GcrQueue::assertAcknowledgedMessagesCount(1);
+        //        GcrQueue::assertMessageHasAcknowledged('1777817206939726');
 
         self::assertEquals('completed', Cache::get('dummy-job-status'));
     }
